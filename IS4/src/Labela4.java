fb42e73d281930c3ff6009460bb12e325db601a5
Vadim <59086091+1Valerion1@users.noreply.github.com>
2022-05-19 16:17:06 +0700

    Update Labela4.java

--- IS4/src/Labela4.java
+++ IS4/src/Labela4.java
@@ -1,57 +1,58 @@
 public class Labela4 {
 	 public static void main(String args[]) {
-		 System.out.println("Програма для преобразования градусов в радианы");
-		 // данные на которые опирается наша нейронная сеть(Учитель)
-		 double[] Degrees = { 1,7,13,15,28,45 };                      // данные в градусах и
-		 double[] Radians = { 0.0174533,0.122173,0.226893,0.261799,0.488692,0.785398 };//  конвертация в радианы
+		 System.out.println("ГЏГ°Г®ГЈГ°Г Г¬Г  Г¤Г«Гї ГЇГ°ГҐГ®ГЎГ°Г Г§Г®ГўГ Г­ГЁГї ГЈГ°Г Г¤ГіГ±Г®Гў Гў Г°Г Г¤ГЁГ Г­Г»");
+		 // РґР°РЅРЅС‹Рµ РЅР° РєРѕС‚РѕСЂС‹Рµ РѕРїРёСЂР°РµС‚СЃСЏ РЅР°С€Р° РЅРµР№СЂРѕРЅРЅР°СЏ СЃРµС‚СЊ(РЈС‡РёС‚РµР»СЊ)
+		 double[] Degrees = { 1,5,13,23,28,48,67};                     // РґР°РЅРЅС‹Рµ РІ РіСЂР°РґСѓСЃР°С… Рё
+		 double[] Radians = { 0.0174533,0.0872665,0.226893,0.401426,0.488692,0.837758,1.16937};//  РєРѕРЅРІРµСЂС‚Р°С†РёСЏ РІ СЂР°РґРёР°РЅС‹
 		 
 		 NeuralNetwork n = new NeuralNetwork();
-		   int r = (int) (Math.random()*6);//выбираем любое из значение и по нему обучаемся
+		   int r = (int) (Math.random()*7);//РІС‹Р±РёСЂР°РµРј Р»СЋР±РѕРµ РёР· 7 Р·РЅР°С‡РµРЅРёРµ Рё РїРѕ РЅРµРјСѓ РѕР±СѓС‡Р°РµРјСЃСЏ
+		   int i = 0;
 		   int i = 0;
 		   while ((n.Error > n.speed)||(n.Error < -n.speed))
 		    {
 		        i++;
 		        n.NeuronTraining(Degrees[r], Radians[r]);
-		        if (i % 10000000 == 0)       // 
+		        if (i % 10000000 == 0)       //РґР»СЏ С‚РѕРіРѕ С‡С‚РѕР±С‹ РІС‹РІРѕРґРёР»Р° РєР°Р¶РґСѓСЋ 10 000 000, Р° РЅРµ РІСЃРµ Р·РЅР°С‡РµРЅРёСЏ
 		        {
 		        	 System.out.println("Error number: "+i+" Radians:"+ n.Error);
 		        }
 		    }
 		 
-		 // System.out.println("\n");
-		    n.ResultCalcNeurons(15);
-		    n.ReverseOutput(1);
-		    n.ResultCalcNeurons(40);
-		    n.ReverseOutput(50);
-		    n.ResultCalcNeurons(3);
-		    n.ReverseOutput(356);
+		  System.out.println("\n");
+                    n.ResultCalcNeurons(666);
+		    n.ReverseOutput(10);
+		    n.ResultCalcNeurons(14);
+		    n.ReverseOutput(666);
+		    n.ResultCalcNeurons(312);
+		    n.ReverseOutput(234);
 		 }
 	}
 	class NeuralNetwork{
 		public
-		double weight = Math.random() * 3; //переписать
-		double Error=-0.0001;//
-		double speed=0.00000001;//  скорость обучения нейрона
+		double weight = Math.random() * 3; 
+		double Error=-0.000001;//
+		double speed=0.00000001;// СЃРєРѕСЂРѕСЃС‚СЊ РѕР±СѓС‡РµРЅРёСЏ РЅРµР№СЂРѕРЅР°
 		double result;
 
-		 public void ResultCalcNeurons(double input)//вывод значений в результатов расчета нейрона(градусы->радианы)
+		 public void ResultCalcNeurons(double input)//РІС‹РІРѕРґ Р·РЅР°С‡РµРЅРёР№ РІ СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ СЂР°СЃС‡РµС‚Р° РЅРµР№СЂРѕРЅР°(РіСЂР°РґСѓСЃС‹->СЂР°РґРёР°РЅС‹)
 		{
 			 System.out.println((int)input+" degress eqals: "+input*weight+" radians");
 		}
 		 
-		public void ReverseOutput(double output)//обратный вывод из радиан->градусы(выход/вес)
+		public void ReverseOutput(double output)//РѕР±СЂР°С‚РЅС‹Р№ РІС‹РІРѕРґ РёР· СЂР°РґРёР°РЅ->РіСЂР°РґСѓСЃС‹(РІС‹С…РѕРґ/РІРµСЃ)
 		{
 			System.out.println((int)output+" radians equals "+output/weight+" degrees");
 		}
 		 
-		public void NeuronTraining(double input, double output)//Тренировка(градусы/радианы)
+		public void NeuronTraining(double input, double output)//РўСЂРµРЅРёСЂРѕРІРєР°(РіСЂР°РґСѓСЃС‹/СЂР°РґРёР°РЅС‹)
 		{
-			double InterResult,correct;//результат на данный момент и размер нужной корректировки
+			double InterResult,correct;//СЂРµР·СѓР»СЊС‚Р°С‚ РЅР° РґР°РЅРЅС‹Р№ РјРѕРјРµРЅС‚ Рё СЂР°Р·РјРµСЂ РЅСѓР¶РЅРѕР№ РєРѕСЂСЂРµРєС‚РёСЂРѕРІРєРё
 			
 			InterResult= input*weight;
-			Error= output-InterResult;//размер ошибки
+			Error= output-InterResult;//СЂР°Р·РјРµСЂ РѕС€РёР±РєРё
 			correct=(Error/InterResult)*speed;
-			weight=weight+correct;// изменение веса на размер корректировки
+			weight=weight+correct;// РёР·РјРµРЅРµРЅРёРµ РІРµСЃР° РЅР° СЂР°Р·РјРµСЂ РєРѕСЂСЂРµРєС‚РёСЂРѕРІРєРё
 		}
 	}
 
